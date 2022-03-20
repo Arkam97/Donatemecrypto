3c99cb8074b8946cac133de6985c6fef114b42f7
Arkam97 <husainmohamedarkam1997@gmail.com>
2022-03-20 21:01:29 +0530

    add changes

--- app.js
+++ app.js
@@ -56,7 +56,11 @@ mongoose
 
 
  if(process.env.NODE_ENV === 'production'){
-   app.use(express.static('Client/build'));   
+   app.use(express.static('Client/build'));  
+   
+   app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
  }
   app.listen(port, () =>
   console.log(`server running on port ${port}`)
